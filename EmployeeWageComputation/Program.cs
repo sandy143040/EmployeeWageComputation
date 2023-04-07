fe6d41c01cd2433320ce20231304f86b6419487f
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-07 15:57:41 +0530

    [sandeep] adding welcome message

---
+++ EmployeeWageComputation/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem statement");
+            Console.ReadLine();
+        }
+    }
+}
