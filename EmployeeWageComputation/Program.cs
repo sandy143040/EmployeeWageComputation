2db8a5863d089e2713b4317b9b7c20e8cd80bf09
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-09 14:46:50 +0530

    [Sandeep] Modify Calculating Wages For a Month

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,38 +8,43 @@ namespace EmployeeWageComputation
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
 
             int empHrs = 0;
-            int empWage = 0;
+            int empWage = 0; 
+            int totalEmpWage = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
+            const int NUM_OF_WORKING_DAYS = 20;
             const int EMP_RATE_PER_HR = 20;
 
             //welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem statement");
-            //UC3- Switch Case Statement
-            Random random = new Random();
-            int employeeAttendence = random.Next(0, 3); //0 , 1 or 2
-            switch (employeeAttendence)
+            //Computation
+            for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_FULL_TIME:
-                    // code block
-                    empHrs = 8;
-                    break;
-                case IS_PART_TIME:
-                    //code block
-                    empHrs = 4;
-                    break;
-                default:
-                    //code block
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+                int employeeAttendence = random.Next(0, 3); //0 , 1 or 2
+                switch (employeeAttendence)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = EMP_RATE_PER_HR * empHrs;
+                totalEmpWage += empWage;
+                Console.WriteLine("Employee wage is : " + empWage);
             }
-               empWage = EMP_RATE_PER_HR * empHrs;
-               Console.WriteLine("Employee wage is:" + empWage);
-               Console.ReadLine();
+                Console.WriteLine("total emp wage : " + totalEmpWage);
+                Console.ReadLine();
         }
     }
 }
