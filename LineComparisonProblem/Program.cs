47b82181a0f04798143af72d9d3624cdf9781577
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-04-11 11:11:44 +0530

    [Sandeep] Modify Calculating Length of Line

---
+++ LineComparisonProblem/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 1.5;
+            double y = 2.5;
+
+            // Define another point to create a line
+            double x2 = 3.0;
+            double y2 = 4.0;
+
+            // Calculate length of line
+            double length = Math.Sqrt(Math.Pow(x2 - x, 2) + Math.Pow(y2 - y, 2));
+
+            // Output result
+            Console.WriteLine("Length of line: " + length);
+            Console.ReadLine();
+        }
+    }
+}
