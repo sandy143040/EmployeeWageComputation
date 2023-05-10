9de5532a45b27a198802462f3294fc9782b16dad
Sandeep kumar singh <sandeeprj793@gmail.com>
2023-05-10 15:15:31 +0530

    [Sandeep] Modify manage emp wage for multiple comp using interface

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -11,30 +11,10 @@ namespace EmployeeWageComputation
         
         static void Main(string[] args)
         {
-           
-            Console.Write("Enter the number of companies: ");
-            int numCompanies = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= numCompanies; i++)
-            {
-                Console.WriteLine($"Enter details for company {i}:");
-
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-
-                Console.Write("Wage per hour: ");
-                int wage = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Working hours per day: ");
-                int hours = Convert.ToInt32(Console.ReadLine());
-
-                Console.Write("Working days per month: ");
-                int days = Convert.ToInt32(Console.ReadLine());
-
-                EmployeeWage.ComputeEmpWage(name, wage, hours, days);
-
-                Console.WriteLine();
-            }
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilderArray.computeEmpWage();
 
             Console.ReadLine();
 
